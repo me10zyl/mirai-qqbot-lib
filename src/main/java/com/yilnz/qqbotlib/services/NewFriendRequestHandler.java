792ba45c9c7584f07b9c5fd94d93c370ed2c7249
astashz <me10zyl@qq.com>
2022-03-25 11:38:25 +0800

    appUtil debug

--- src/main/java/com/yilnz/qqbotlib/services/NewFriendRequestHandler.java
+++ src/main/java/com/yilnz/qqbotlib/services/NewFriendRequestHandler.java
@@ -10,6 +10,10 @@ import com.yilnz.qqbotlib.util.ApiUtil;
 import com.yilnz.surfing.core.basic.Json;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 //[{"nick":"zSkyRaker","eventId":1648178457000000,"groupId":829987644,"type":"NewFriendRequestEvent","message":"","fromId":602077597}]
 public class NewFriendRequestHandler implements MsgJsonHandler {
@@ -39,7 +43,12 @@ public class NewFriendRequestHandler implements MsgJsonHandler {
             apiUtil.handleNewFriendRequestEvent(json);
             List<QQMessage> qqMessageList = listener.postReceivedNewFriendRequest(friendRequest);
             if(qqMessageList != null) {
-                apiUtil.sendFriendMessage(String.valueOf(friendRequest.getFromId()), qqMessageList);
+                new ScheduledThreadPoolExecutor(1).schedule(new Runnable() {
+                    @Override
+                    public void run() {
+                        apiUtil.sendFriendMessage(String.valueOf(friendRequest.getFromId()), qqMessageList);
+                    }
+                }, 5, TimeUnit.SECONDS);
             }
         }
     }
